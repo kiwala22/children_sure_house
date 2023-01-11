f6d793feba8d775bef4d76c864c1c3dfa01a7e99
paulgrammer <paulmugaya@live.com>
2023-01-11 17:19:39 +0300

    news feed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_06_090910) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_06_111518) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -73,6 +73,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_06_090910) do
     t.string "name"
     t.string "subject"
     t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "news_feeds", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
